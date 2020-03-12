67b47e53c6d4365b1cd4648a5834078ad6ef57bd
Lei <ylxx@live.com>
2020-03-12 11:53:13 +0800

    update 6d_pose_demo

--- example/6d_pose_demo.py
+++ example/6d_pose_demo.py
@@ -8,17 +8,16 @@ Run Command:
     blender --background --python 6d_pose_demo.py
 """
 
-from boxx import *
-from boxx import np, npa, getDefaultColorList, pi
-
 import cv2
 import bpy
 import bpycv
+import boxx
 import random
+import numpy as np
 import scipy.io as sio
 
 
-def vis_pose(img, xyzs, pose, K, color=None):
+def _vis_pose(img, xyzs, pose, K, color=None):
     xyz2 = np.dot(xyzs, pose[:, :3].T) + pose[:, 3]
     p_p3d = np.dot(xyz2, K.T)
     p_p3d[:, 0] = p_p3d[:, 0] / p_p3d[:, 2]
@@ -31,30 +30,27 @@ def vis_pose(img, xyzs, pose, K, color=None):
 
 def vis_poses_by_ycb_6d_pose(img, meta, xyzs=None):
     n = meta["poses"].shape[-1]
-    colors = npa(getDefaultColorList(n + 3)) * 255
+    colors = np.array(boxx.getDefaultColorList(n + 3)) * 255  # get some random colors
     for idx in range(n):
         pose = meta["poses"][:, :, idx]
         K = meta["intrinsic_matrix"]
         if xyzs is None:
             xyzs = meta.get("bound_boxs")[idx]
-        img = vis_pose(img, xyzs, pose, K, colors[idx + 1])
+        img = _vis_pose(img, xyzs, pose, K, colors[idx + 1])
     return img
 
 
 # remove all MESH objects
 [bpy.data.objects.remove(obj) for obj in bpy.data.objects if obj.type == "MESH"]
 
-cube_size = 1
 for inst_id in range(1, 3):
     location = [random.random() * 4 - 2 for _ in range(3)]
-    rotation = [random.random() * 2 * pi for _ in range(3)]
+    rotation = [random.random() * 2 * np.pi for _ in range(3)]
     # add cube
-    bpy.ops.mesh.primitive_cube_add(
-        size=cube_size, location=location, rotation=rotation
-    )
-    obj = bpy.context.active_object
+    bpy.ops.mesh.primitive_cube_add(size=1, location=location, rotation=rotation)
+    cube = bpy.context.active_object
     # set each instance a unique inst_id, which is used to generate instance annotation.
-    obj["inst_id"] = inst_id
+    cube["inst_id"] = inst_id
 
 result = bpycv.render_data()
 
@@ -62,25 +58,14 @@ result = bpycv.render_data()
 meta = result["ycb_6d_pose"]
 img = result["image"]
 
-# cube mesh xyz
-cube_xyz = np.array(
-    [
-        [1.0, 1.0, 1.0],
-        [1.0, 1.0, -1.0],
-        [1.0, -1.0, 1.0],
-        [-1.0, 1.0, 1.0],
-        [1.0, -1.0, -1.0],
-        [-1.0, 1.0, -1.0],
-        [-1.0, -1.0, 1.0],
-        [-1.0, -1.0, -1.0],
-    ]
-)
-cube_xyz *= cube_size / 2
+# all vertices in cube
+cube_xyz = [list(v.co) for v in cube.data.vertices]
 
 vis = vis_poses_by_ycb_6d_pose(img, meta, cube_xyz)
 
 cv2.imwrite(
     "demo-vis_6d_pose.jpg", cv2.cvtColor(vis, cv2.COLOR_RGB2BGR)
 )  # cover RGB to BGR
+
 sio.savemat("ycb_6d_pose.mat", result["ycb_6d_pose"])
-print("Saving vis image to:", "demo-vis_6d_pose.jpg")
+print("Saving vis image to:", "./demo-vis_6d_pose.jpg")
