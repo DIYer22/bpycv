f91373855c1fb6534fd240461af76ab0076f8330
Lei <ylxx@live.com>
2020-10-21 15:57:50 +0800

    u

--- example/ycb_demo.py
+++ example/ycb_demo.py
@@ -11,6 +11,7 @@ from pathlib import Path
 example_data_dir = Path(
     os.path.abspath(os.path.join(__file__, "../../../bpycv_example_data"))
 )
+# example_data_dir = Path('/home/dl/mygit/bpycv_example_data')
 
 models = glob.glob(str(example_data_dir / "models" / "*" / "*.obj"))
 cat_id_to_model_path = dict(enumerate(sorted(models), 1))
@@ -31,13 +32,17 @@ bpycv.load_hdri_world(hdri_path, random_rotate_z=True)
 
 
 for index in range(6):
-    location = [random.random() * 1 - 0.5 for _ in range(3)]
     cat_id = random.choice(list(cat_id_to_model_path))
     model_path = cat_id_to_model_path[cat_id]
     obj = bpycv.load_obj(model_path)
+    location = (
+        random.uniform(-0.2, 0.2),
+        random.uniform(-0.2, 0.2),
+        random.uniform(0.1, 0.3),
+    )
+    obj.location = location
     # set each instance a unique inst_id, which is used to generate instance annotation.
     obj["inst_id"] = cat_id * 1000 + index
-
     with bpycv.activate_obj(obj):
         bpy.ops.rigidbody.object_add()
 
